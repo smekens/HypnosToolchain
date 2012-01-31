9cfd80b76e12e1397991457c10e1f7fcfcd54129
smekens <smekens@debian.localdomain>
2012-01-31 15:30:32 -0500

    work

--- scripts/download.sh
+++ scripts/download.sh
@@ -12,23 +12,25 @@ mkdir -p $HYPNOS_TOOLCHAIN/srcs
 
 cd $HYPNOS_TOOLCHAIN/srcs
 
-#curl -O $bash_miror
-#curl -O $sed_miror
-#curl -O $grep_miror
-#curl -O $make_miror
-#curl -O $flex_miror
+curl -O $bash_miror
+curl -O $sed_miror
+curl -O $grep_miror
+curl -O $make_miror
+curl -O $flex_miror
 curl -O $byacc_miror
-#curl -O $coreutils_miror
+curl -O $coreutils_miror
 
 #curl -O $llvm_miror
 
-#curl -O $binutils_miror
-#curl -O $gmp_miror
-#curl -O $mpfr_mirror
-#curl -O $mpc_miror
-#curl -O $gcc_core_miror
-#curl -O $gcc_gpp_miror
-#curl -O $gcc_objc_miror
+curl -O $gmp_miror
+curl -O $mpfr_mirror
+curl -O $mpc_miror
+
+curl -O $binutils_miror
+
+curl -O $gcc_core_miror
+curl -O $gcc_gpp_miror
+curl -O $gcc_objc_miror
 
 cd $BASE
 
